5dc5e94ec2011c3c0df6b657145874ecaf1bb655
Nick Hussin <ndhussin@keller-na.com>
2023-03-23 20:37:48 -0400

    Update d.ts

--- svg-gauge.d.ts
+++ svg-gauge.d.ts
@@ -1,61 +1,61 @@
-declare module 'svg-gauge' {
+declare module 'ndh-svg-gauge' {
   export interface GaugeInstance {
-    setMaxValue: (max: number) => void
-    setValue: (val: number) => void
-    setValueAnimated: (val: number, duration: number) => void
-    getValue: () => number
+    setMaxValue: (max: number) => void;
+    setValue: (val: number) => void;
+    setValueAnimated: (val: number, duration: number) => void;
+    getValue: () => number;
   }
 
   export interface GaugeOptions {
     /** The angle in degrees to start the dial
      * @default 135
      */
-    dialStartAngle?: number
+    dialStartAngle?: number;
     /** The angle in degrees to end the dial. This MUST be less than dialStartAngle
      * @default 45
      */
-    dialEndAngle?: number
+    dialEndAngle?: number;
     /** The radius of the gauge
      * @default 40
      */
-    dialRadius?: number
+    dialRadius?: number;
     /** The minimum value for the gauge. This can be a negative value
      * @default 0
      */
-    min?: number
+    min?: number;
     /** The maximum value for the gauge
      * @default 100
      */
-    max?: number
+    max?: number;
     /** Getter for the label that will be rendered in the center. */
-    label?: (currentValue: number) => string
+    label?: (currentValue: number) => string;
     /** Whether to show the value at the center of the gauge
      * @default true
      */
-    showValue?: boolean
+    showValue?: boolean;
     /** The CSS class of the gauge
      * @default 'gauge'
      */
-    gaugeClass?: string
+    gaugeClass?: string;
     /** The CSS class of the gauge's dial
      * @default 'dial'
      */
-    dialClass?: string
+    dialClass?: string;
     /** The CSS class of the gauge's fill
      * @default 'value'
      */
-    valueDialClass?: string
+    valueDialClass?: string;
     /** The CSS class of the gauge's text
      * @default '(value-text)'
      */
-    valueClass?: string
+    valueClass?: string;
     /** An optional function that can return a color for current value
      */
-    color?: (currentValue: number) => string
+    color?: (currentValue: number) => string;
     /** An optional string that specifies the crop region
      * @default '(0 0 100 100)'
      */
-    viewBox?: string
+    viewBox?: string;
   }
 
   /**
@@ -63,7 +63,10 @@ declare module 'svg-gauge' {
    * @param {Element} element The DOM into which to render the gauge
    * @param {GaugeOptions} options The gauge options
    */
-  const createInstance: (element: Element, options?: GaugeOptions) => GaugeInstance
+  const createInstance: (
+    element: Element,
+    options?: GaugeOptions
+  ) => GaugeInstance;
 
-  export default createInstance
+  export default createInstance;
 }
